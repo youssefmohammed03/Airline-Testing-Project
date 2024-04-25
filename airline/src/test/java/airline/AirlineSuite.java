31f00b821680b1b4d2260520dd3be93e36140644
Abdallah Ahmed <21p0333@eng.asu.edu.eg>
2024-04-25 17:43:54 +0200

    TestSuites Fully Done by Abdallah

--- airline/src/test/java/airline/AirlineSuite.java
+++ airline/src/test/java/airline/AirlineSuite.java
@@ -6,6 +6,5 @@ import org.junit.platform.suite.api.SelectClasses;
 @Suite
 @SelectClasses({AdminTest.class, AirlineTest.class, LoginSignupTest.class, PassengerTicketTest.class})
 public class AirlineSuite {
-    // This class doesn't have any code because it's just a container for the test suite.
-    // You can add methods or other configurations if needed.
+
 }
